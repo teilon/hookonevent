b97a2758b225e7c5733818e4a694198ccea4b803
Teilon <teiloncw@gmail.com>
2017-03-29 18:19:04 +0600

    add transport dictionary

--- calcevent/progress/TransportDict.cs
+++ calcevent/progress/TransportDict.cs
@@ -8,6 +8,14 @@ namespace calcevent.progress
 {
     public class TransportDict
     {        
-        public Dictionary<string, Dictionary<string, string>> TransportItem = new Dictionary<string, Dictionary<string, string>>();
+        public Dictionary<string, Dictionary<string, string>> Item = new Dictionary<string, Dictionary<string, string>>();
+
+
+        string pattern_fort = "deviceID:[\"'][\\w\\d_-]+[\"'],timestamp:\\d+,statusCode:[\\w\\d_-]+,latitude:\\d+\\.?\\d+,longitude:\\d+\\.?\\d+,speedKPH:\\d+\\.?\\d*,heading:\\d+\\.?\\d*,altitude:\\d+\\.?\\d*";
+        string pattern_tabl = "(deviceID|transportID):[\"'][\\w\\d_-]+[\"'],timestamp:\\d+,(driverMessage|transportStatus):\\d{4},oreType:\\d+";
+        public void AddMessage(string input)
+        {
+
+        }
     }
 }
