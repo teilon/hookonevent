377c8d7679dbc136dbfe4a920f52f3520a33a13a
Teilon <teiloncw@gmail.com>
2017-04-27 18:12:33 +0600

    add test files

---
+++ calcevent/statemachine/atom/StateConfigurationDict.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calcevent.status;
+
+namespace calcevent.statemachine.atom
+{
+    class StateConfigurationDict
+    {
+        
+    }
+    /*
+    class Rule : IDictionary<State, RuleSet>
+    {
+
+    }
+    */
+    class RuleSet : ICollection<KeyValuePair<Trigger, State>>
+    {
+        int _count = 0;
+        bool _isreadonly = true;
+        public int Count { get { return _count; } }
+        public bool IsReadOnly { get { return _isreadonly; } }
+
+        public void Add(KeyValuePair<Trigger, State> item)
+        {
+            this.Add(item);
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<Trigger, State> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<Trigger, State>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<Trigger, State>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<Trigger, State> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- testApp/testApp/Test.cs
+++ testApp/testApp/Test.cs
@@ -20,35 +20,29 @@ namespace testApp
         }
         public void Start()
         {
-            /*
-            TestTransports();
-            
-            TestZones();
-            
-            TestStates();
-
-            TransportProgress["804"].CurrentState.ToMove();
-            TransportProgress["806"].CurrentState.ToMove();
-            TransportProgress["809"].CurrentState.ToMove();
-            TransportProgress["804"].CurrentState.ToMove();
-            TransportProgress["10"].CurrentState.ToMove();
-            TransportProgress["806"].CurrentState.ToStop();
-            TransportProgress["804"].CurrentState.ToStop();
-            TransportProgress["806"].CurrentState.ToMove();
-            
-            TestStates();
-            */
-
             List<string> messages = new List<string>();
-            messages.Add("deviceID:'804',timestamp:1654892,statusCode:60012,latitude:43.568945,longitude:72.35687,speedKPH:2,heading:12.0,altitude:120");
-            messages.Add("deviceID:'804',timestamp:1654902,statusCode:60012,latitude:43.568966,longitude:72.35632,speedKPH:2,heading:12.0,altitude:120");
-            messages.Add("deviceID:'804',timestamp:1654912,statusCode:60012,latitude:43.568966,longitude:72.35632,speedKPH:0,heading:12.0,altitude:120");
-            messages.Add("deviceID:'804',timestamp:1654922,statusCode:60012,latitude:43.569002,longitude:72.35655,speedKPH:2,heading:12.0,altitude:120");
-            messages.Add("deviceID:'804',timestamp:1654932,statusCode:60012,latitude:43.569123,longitude:72.35601,speedKPH:2,heading:12.0,altitude:120");
 
-            TestSyncZoneExcv();
+            
+
+            messages.Add("deviceID:'804',timestamp:1492662332,statusCode:63645,latitude:43.235660807291666,longitude:76.87373860677083,speedKPH:0.0,heading:0.0,altitude:822.0");
+            messages.Add("deviceID:'804',timestamp:1492662337,statusCode:63645,latitude:43.235742187499994,longitude:76.87369791666667,speedKPH:13.1,heading:0.0,altitude:822.0");
+            messages.Add("deviceID:'804',timestamp:1492662347,statusCode:63645,latitude:43.235449218750006,longitude:76.87360026041668,speedKPH:19.5,heading:0.0,altitude:821.0");
+            messages.Add("deviceID:'804',timestamp:1492758514,statusCode:63645,latitude:43.235660807291666,longitude:76.87373860677083,speedKPH:0.0,heading:0.0,altitude:822.0");
+            messages.Add("deviceID:'111',timestamp:1492758510,transportStatus:1011,oreType:2");
+            messages.Add("deviceID:'804',timestamp:1492662357,statusCode:63645,latitude:43.23486328125001,longitude:76.87309570312499,speedKPH:51.3,heading:0.0,altitude:823.0");
+            messages.Add("deviceID:'804',timestamp:1492662378,statusCode:63645,latitude:43.231127929687496,longitude:76.86805826822918,speedKPH:59.3,heading:0.0,altitude:832.0");
+            messages.Add("deviceID:'804',timestamp:1492662404,statusCode:63645,latitude:43.2277587890625,longitude:76.863623046875,speedKPH:1.4,heading:0.0,altitude:844.0");
+            messages.Add("deviceID:'804',timestamp:1492662414,statusCode:63645,latitude:43.229101562500006,longitude:76.86548665364585,speedKPH:72.0,heading:0.0,altitude:837.0");
+
+            //TestSyncZoneExcv();
+            System.Diagnostics.Debug.WriteLine("\nstart()\n");
+            string responce = string.Empty;
             foreach(var s in messages)
-                recipient.AddMessage(s);
+            {
+                responce = recipient.AddMessage(s);
+                System.Diagnostics.Debug.WriteLine(responce);
+            }
+                
         }        
         void TestSyncZoneExcv()
         {
